ec561c71ac892d99a85e76fc776b89129bcbe9ad
Yuliana-vish <yuliana.vish@gmail.com>
2021-11-08 16:47:00 +0200

    add function and styles

---
+++ src/interfaces.ts
@@ -0,0 +1,29 @@
+export interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface WeatherData {
+  lon: number;
+  lat: number;
+  timezone: number;
+  timezone_offset: number;
+  current: {
+    clouds: number;
+    dew_point: number;
+    dt: number;
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    sunrise: number;
+    sunset: number;
+    visibility: number;
+    uvi: number;
+    weather: [Weather];
+    wind_speed: number;
+    wind_deg: number;
+  };
+}
