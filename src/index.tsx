6743be9e806c59c3a013551e176d46cc3081e64c
Yuliana-vish <yuliana.vish@gmail.com>
2021-11-11 13:35:15 +0200

    add changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as firebase from 'firebase/app';
-
-firebase.initializeApp({
-  apiKey: 'AIzaSyBB3QUPwuAq8isKCO68xfCT-3PXkWimC2w',
-  authDomain: 'weather-app-52604.firebaseapp.com',
-  projectId: 'weather-app-52604',
-  storageBucket: 'weather-app-52604.appspot.com',
-  messagingSenderId: '273700270071',
-  appId: '1:273700270071:web:27ac3f2414187fb47e08f2',
-});
 
 ReactDOM.render(
   <React.StrictMode>
